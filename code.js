fd1736be19d779f665be9264c6270175c13e543d
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-03-11 00:13:15 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,43 @@
 function flip(array, n) {
-    return array;
+    var Ostack = [];
+    if (n > array.length) {
+        var Istack = [];
+        var arrLen = array.length
+        for (i = 0; i <= arrLen - 1; i++) {
+            Istack.push(array[array.length - 1])
+            array.pop()
+        }
+        return Istack;
+    }
+    var B = array.slice(0, n)
+    for (i = 0; i < n; i++) {
+        Ostack.push(B[B.length - 1])
+        B.pop()
+    }
+    for (i > n; i < array.length; i++) {
+        Ostack.splice(i, 0, array[i]);
+    }
+    return Ostack;
 }
-
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
-    return array;
+    function pancakeSort_(array,ArrayLen){
+        var arrPop=array.slice(0,ArrayLen+1)
+        var max = Math.max(...arrPop);
+        var Flipindex=array.indexOf(max)
+        var Flipindex = ArrayLen - arrPop.indexOf(max);
+        if (ArrayLen == 1 && array[0]==Math.min(...arrPop)) {
+            return array;
+        }
+        // if(max==array[1]){
+        //     return pancakeSort_(flip(array,ArrayLen))
+        // }
+        if (Flipindex == 0) {
+            ArrayLen -= 1;
+            return pancakeSort_(array,ArrayLen);
+        }
+        var fliparr=flip(array,Flipindex+1);
+        return pancakeSort_(fliparr,ArrayLen)
+    };
+        return pancakeSort_(array,array.length-1)
 }
